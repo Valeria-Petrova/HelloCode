bf58919618e3f9aa0c17cfbe514f1a665d0ffa40
Valeria <valeria89_07@mail.ru>
2022-12-03 11:56:45 +0300

    сделали if else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() =="маша") //позволяет перевести все буквы в нижний регистр
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
