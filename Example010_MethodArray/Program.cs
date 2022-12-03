ba4b8b0db54328bcfa58dc775940e4a9902021ad
Valeria <valeria89_07@mail.ru>
2022-12-03 15:12:14 +0300

    метод массив

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿//задача:одномерный массив array из n эл-в, требуется найти эл-т массива равный find
+int[] array = { 11, 81, 21, 31, 41, 15, 16, 17, 81, 19 };
+
+int n = array.Length;
+int find = 81;
+
+int index = 0;
+
+while (index < n)
+{
+   if(array[index] == find)
+   {
+    Console.WriteLine(index);
+    break;
+   }
+
+    //index = index +1;
+    index++;
+}
