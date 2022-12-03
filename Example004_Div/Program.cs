11ba6c810269bbbc595d8d95d67ad243759ec4db
Valeria <valeria89_07@mail.ru>
2022-12-03 11:36:14 +0300

    random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1,10); // случайное число будет 1,2,3....9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
 
